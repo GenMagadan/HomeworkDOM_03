087e2ffbe16ee7ab9401b0873422e694d1501463
GenMagadan <genchik111@yandex.ru>
2023-12-06 15:36:47 +0300

    Add task_4

--- script.js
+++ script.js
@@ -17,27 +17,31 @@ window.addEventListener('load', () => {
 // "super_element" у элемента, а также выводить в нижнем регистре верный тег в данной строке,
 // по которому был совершен клик.
 // - Необходимо использовать делегирование.
-document.addEventListener('click', event => {
+document.addEventListener('click', e => {
    let text = '';
-   const classSEL = event.target.classList.contains('super_element');
+   const classSEL = e.target.classList.contains('super_element');
    if (classSEL == true) {
       text = 'присутствует';
    } else {
       text = 'отсутствует';
    }
-   console.log(`Класс "super_element" ${text} в элементе "${event.target.tagName.toLowerCase()}".`);
+   console.log(`Класс "super_element" ${text} в элементе "${e.target.tagName.toLowerCase()}".`);
 });
 // 4. Сделайте, чтобы при наведении на textarea в консоли появлялось сообщение:
 // "Вы навели на textarea."
+document.querySelector('textarea').addEventListener('mouseover', () =>
+   console.log("Вы навели на textarea."));
 
 // 5. Необходимо повесить событие клика на тег ul.В обработчике события в
 // консоль необходимо выводить текст, который записан внутри элемента кнопки,
-// по которой был произведен клик.Если клик был не по кнопке, то ничего выводить
-//  не нужно.
-//  Необходимо использовать делегирование.
+// по которой был произведен клик.Если клик был не по кнопке, то ничего
+// выводить не нужно.
+// Необходимо использовать делегирование.
 
 // 6. Вопрос: Почему в console.log пишется сначала текст из 5 задания и только
 // потом выводится текст из 3 задания, если мы кликаем по кнопкам в ul ? Ответ
 // необходимо написать здесь же, под этим комментарием, своими словами.
 
+
 // 7. С помощью JS необходимо изменить цвет заднего фона каждого второго тега li.
+
